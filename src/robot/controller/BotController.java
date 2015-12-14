2617b15ef9e26b7e4ca36ab5a0f32e57d86e6c7c
ForrestThacker <fyruskasai@gmail.com>
2015-12-14 13:48:41 -0700

    Added to the repo.

--- src/robot/controller/BotController.java
+++ src/robot/controller/BotController.java
@@ -1,6 +1,42 @@
 package robot.controller;
 
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+import robot.model.EV3Bot;
+
+/**
+ * 
+ * @author ftha4681
+ * @version 0.x Dec 10, 2015
+ */
+
 public class BotController 
 {
-
+	private String message;
+	private int xPosition;
+	private int yPosition;
+	
+	private long waitTime;
+	
+	private EV3Bot forBot;
+	
+	public BotController()
+	{
+		this.message = "Programming Lego robots with Java";
+		this.xPosition = 10;
+		this.yPosition = 10;
+		this.waitTime = 4000;
+		
+		forBot = new EV3Bot();
+	}
+	
+	public void start()
+	{
+		LCD.drawString(botMessage, xPosition, yPosition);
+		Delay.msDelay(waitTime);
+	}
+	
+		forBot.driveRoom();
+	
 }
---
+++ src/robot/model/EV3Bot.java
@@ -0,0 +1,28 @@
+package robot.model;
+
+public class EV3Bot 
+{
+	private String botMessage;
+	private int xPosition;
+	private int yPosition;
+	private long waitTime;
+	
+	public EV3Bot()
+	{
+		this.botMessage = "Forrest codes forBot";
+		this.xPosition = 50;
+		this.yPosition = 50;
+		this.waitTime = 4000;
+	}
+	
+	public void driveRoom()
+	{
+		//call private helper method here
+	}
+	
+	private void displayMessage()
+	{
+	//write your first drive method here
+	//call the displayMessage("describe action as a string") in the helper method
+	}
+}
